cd9540600917233022088e95a9d72488b06ce3b8
Manu2021-U <manu.mudavath03@gmail.com>
2025-05-29 20:50:14 +1200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
                         <HiMiniBars3CenterLeft className="size-6" />
                     </Link>
 
-                    {/* search input */}
+                    {/* search input 
                     <div className="relative sm:w-72 w-40 space-x-2">
 
                         <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
@@ -45,7 +45,7 @@ const Navbar = () => {
                         <input type="text" placeholder="Search here"
                             className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
                         />
-                    </div>
+                    </div> */}
                 </div>
 
 
